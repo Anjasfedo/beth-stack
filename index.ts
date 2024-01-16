0fa7d5357107f1c8f811b0a0e39f5a48a6f0f9a5
AnjasFedo <fedoaf98@gmail.com>
2024-01-16 18:59:23 +0700

    Add Elysia

--- index.ts
+++ index.ts
@@ -1,1 +1,5 @@
-console.log("Hello via Bun!");
+import { Elysia } from "elysia";
+
+const app = new Elysia().get("/", () => "Anjasfedo").listen(3000);
+
+console.log(`Elysia is running at http://${app.server?.hostname}:${app.server?.port}`)
