961d9f96e30e67ee0312853341138e5b3b54c433
AnjasFedo <fedoaf98@gmail.com>
2024-01-16 19:19:39 +0700

    Add HTMX

--- index.tsx
+++ index.tsx
@@ -1,12 +1,19 @@
 import { html } from "@elysiajs/html";
+
 import { Elysia } from "elysia";
+
+import * as elements from "typed-html";
 
 const app = new Elysia()
   .use(html())
-  .get(
-    "/",
-      ({ html }) =>
-        html(baseHTML)
+  .get("/", ({ html }) =>
+    html(
+      <BaseHTML>
+        <body>
+          <h1>Anjas</h1>
+        </body>
+      </BaseHTML>
+    )
   )
   .listen(3000);
 
@@ -14,15 +21,15 @@ console.log(
   `Elysia is running at http://${app.server?.hostname}:${app.server?.port}`
 );
 
-const baseHTML = `
+const BaseHTML = ({ children }: elements.Children) => `
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BETH Stack</title>
+    <script src="https://unpkg.com/htmx.org@1.9.10"></script>
 </head>
-<body>
-    <h1>Anjasfedo</h1>
-</body>
-</html>`;
+${children}
+</html>
+`;
