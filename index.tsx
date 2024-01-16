ff1e3943008b8fb253c52537421f32fe1d88addd
AnjasFedo <fedoaf98@gmail.com>
2024-01-16 19:51:37 +0700

    Return Response JSX Component

--- index.tsx
+++ index.tsx
@@ -9,7 +9,7 @@ const app = new Elysia()
   .get("/", ({ html }) =>
     html(
       <BaseHTML>
-        <body>
+        <body class="flex w-full h-screen justify-center items-center bg-slate-900 text-white text-2xl">
           <button hx-post="/clicked" hx-swap="outerHTML">
             Click Me
           </button>
@@ -17,12 +17,11 @@ const app = new Elysia()
       </BaseHTML>
     )
   )
-  .post("/clicked", () => <div>Anjas</div>)
-  .listen(3000);
-
-console.log(
-  `Elysia is running at http://${app.server?.hostname}:${app.server?.port}`
-);
+  .post("/clicked", () => <div class="text-violet-400">Anjas</div>)
+  .get("todos", () => <TodoList todos={todos} />)
+  .listen(3000, ({ hostname, port }) =>
+    console.log(`🦊 Elysia running on http://${hostname}:${port}`)
+  );
 
 const BaseHTML = ({ children }: elements.Children) => `
 <!DOCTYPE html>
@@ -37,3 +36,42 @@ const BaseHTML = ({ children }: elements.Children) => `
 ${children}
 </html>
 `;
+
+type Todo = {
+  id: number;
+  content: string;
+  completed: boolean;
+};
+
+const todos: Todo[] = [
+  {
+    id: 1,
+    content: "learn beth",
+    completed: true,
+  },
+  {
+    id: 2,
+    content: "learn golang",
+    completed: false,
+  },
+];
+
+function TodoItem({id, content, completed}: Todo) {
+  return (
+    <div class="flex flex-row space-5-x">
+      <p>{content}</p>
+      <input type="checkbox" checked={completed} />
+      <button class="text-red-500">X</button>
+    </div>
+  )
+}
+
+function TodoList({todos}: {todos: Todo[]}) {
+  return (
+    <div>
+      {todos.map((todo) => (
+        <TodoItem {...todo} />
+      ))}
+    </div>
+  )
+}
