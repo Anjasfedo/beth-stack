a677e7df3b673db48968e317f21678f3018a2ca7
AnjasFedo <fedoaf98@gmail.com>
2024-01-16 19:09:33 +0700

    Return HTML Page to Client

--- index.ts
+++ index.ts
@@ -1,5 +1,28 @@
+import { html } from "@elysiajs/html";
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Anjasfedo").listen(3000);
+const app = new Elysia()
+  .use(html())
+  .get(
+    "/",
+      ({ html }) =>
+        html(baseHTML)
+  )
+  .listen(3000);
 
-console.log(`Elysia is running at http://${app.server?.hostname}:${app.server?.port}`)
+console.log(
+  `Elysia is running at http://${app.server?.hostname}:${app.server?.port}`
+);
+
+const baseHTML = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BETH Stack</title>
+</head>
+<body>
+    <h1>Anjasfedo</h1>
+</body>
+</html>`;
